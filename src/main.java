1540fb5cdf9234d23fec49c8eb101d4f417bd3e7
Dderda1 <31541553+dderda1@users.noreply.github.com>
2018-03-09 08:45:00 -0600

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,6 +1,13 @@
-public class main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
-    }
-}
+background: #000000
+postbg: #282828
+text: #ffffff
+chat1: #7e7e7e
+link: #ffffff
+linkhover: #dcbb14
+blockquote: #000000
+scrollbar: #d5d5d5
+permalink: #000000
+wrap: #c9bc00
+border: #202020
+highlight: #eeeeee
+text when highlight: #868686
